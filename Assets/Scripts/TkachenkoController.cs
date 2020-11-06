5e2df7e2ad44a34b4a0585a58f6fc8938584cf39
A-L-T <alt@alternative.ms>
2020-11-06 12:43:41 +0200

    Scene update

---
+++ Assets/Scripts/TkachenkoController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TkachenkoController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _handsHolder;
+
+    [SerializeField]
+    private float _rotateSpeed = 50f;
+
+    void Start()
+    {
+        Debug.Log("Alexander Tkachenko greets you!");
+    }
+
+    private void Update()
+    {
+        _handsHolder.Rotate(new Vector3(Time.deltaTime * _rotateSpeed, 0, 0), Space.Self);
+    }
+}
