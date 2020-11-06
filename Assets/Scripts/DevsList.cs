58eacf346cdf87f90abbb822f84f201016638d26
Sorpov <vpekuc@gmail.com>
2020-11-06 14:33:25 +0200

    Rename

---
+++ Assets/Scripts/DevsList.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DevsList : MonoBehaviour {
+    private string[] _developers = {
+        "Vadim Kovalchuk",
+        
+    };
+
+    private void Start() {
+        Debug.LogWarning("Who is breathtaking?");
+        foreach (string dev in _developers) Debug.LogWarning(dev + " is breathtaking!");
+    }
+
+    private void Update() {
+        
+    }
+
+}
