7e5d97b0a05452b3b67f50ffd440bd1f88931811
Sorpov <vpekuc@gmail.com>
2020-11-06 12:49:48 +0200

    Added StudentsList controller

---
+++ Assets/Scripts/StudentsList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentsList : MonoBehaviour {
+    private string[] _students = {
+        "Vadim Kovalchuk",
+        
+    };
+
+    private void Start() {
+        foreach (string student in _students) Debug.LogWarning("Student: " + student);
+    }
+
+}
